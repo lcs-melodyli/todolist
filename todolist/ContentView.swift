1a29645d93067becae33f8e662009532c165c688
Melody Li <zli@lcs.on.ca>
2024-04-05 09:50:35 -0400

    Initial Commit

---
+++ todolist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  todolist
+//
+//  Created by 利梓萌 on 2024-04-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by 利梓萌 on 2024-04-05.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
