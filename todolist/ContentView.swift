78564c04012a77715508b28d0bb3356e9381bf9e
Melody Li <zli@lcs.on.ca>
2024-04-05 09:54:39 -0400

    111111111111111111111111111111111111111111111111 Initial code entered

--- todolist/ContentView.swift
+++ todolist/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var newItemDescription: String = ""
+    @State var searchText = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            VStack{
+                List{
+                    
+                }
+                .searchable(text: $searchText)
+                HStack{}
+                    .padding(20)
+            }
+            .navigationTitle("To do")
         }
-        .padding()
     }
 }
 
